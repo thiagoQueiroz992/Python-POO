ccf47b85ab7d25c7750917b0786b8a14662fb769
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-13 08:24:16 -0300

    Employee() class complete

--- ex016/ex016_funcionario.py
+++ ex016/ex016_funcionario.py
@@ -1,3 +1,12 @@
+from rich import print
+
 class Employee():
     def __init__(self, name: str, sector: str, position: str):
-        pass
+        self.name = name
+        self.sector = sector
+        self.position = position
+        
+        self.company = 'Curso em Vídeo'
+    
+    def presenting(self):
+        return f':handshake: Olá, sou [blue]{self.name}[/] e sou {self.position} no setor de {self.sector} da empresa {self.company}.'
