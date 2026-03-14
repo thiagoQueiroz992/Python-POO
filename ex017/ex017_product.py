0cfc90d229d71c93bdfe1b1e6df7d7827358d5e4
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-13 21:36:49 -0300

    Product() class v01

---
+++ ex017/ex017_product.py
@@ -0,0 +1,14 @@
+#from rich import print
+from rich.panel import Panel
+from rich.align import Align
+
+#class Product():
+#    def __init__(self, name: str, price: float):
+#        self.name = name
+#        self.price = price
+#
+#    def label(self):
+#        Panel(Align(str(self.name).center(50) + '\n' + '-' * 50 + '\n' + f'R${self.price:^-50.2f}', align='center'), width=50)
+#
+
+print(f'R${34:^-50.2f}')
