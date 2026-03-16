a438c00458383ebc47d9aa47998c4e98fba6b83a
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-16 18:02:16 -0300

    idk

--- ex019/ex019_livro.py
+++ ex019/ex019_livro.py
@@ -5,3 +5,22 @@ class Book():
         self.book_name = book_name
         self.pages = pages
         self.current_page = 1
+
+    def advance_pages(self, pages = 1):
+        pages_advanced = 0
+        for p in range(pages):
+            if pages_advanced == pages:
+                print(f'Você avançou {pages_advanced} páginas e agora está na página {self.current_page}.')
+                break
+            else:
+                self.current_page += 1
+                pages_advanced += 1
+                print(f'Página {self.current_page}')
+        if self.current_page >= self.pages:
+            print(f'Você chegou ao final do livro {self.book_name}')
+
+book = Book('Teste', 20)
+book.advance_pages(3)
+book.advance_pages(5)
+book.advance_pages()
+book.advance_pages(30)
