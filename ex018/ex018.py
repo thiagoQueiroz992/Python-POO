86136ae4bf94ad9bff7a925109be5766e72b6d8b
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-16 09:18:42 -0300

    classes non tested created

--- ex018/ex018.py
+++ ex018/ex018.py
@@ -3,4 +3,19 @@ from rich.panel import Panel
 
 class Churrasco():
     def __init__(self, name: str, quant: int):
-        pass
+        self.name = name
+        self.quant = quant
+
+        self.consumption_per_person = 0.4
+        self.meat_price = 82.40
+    
+    def analysis(self):
+        quant_to_be_bought = self.consumption_per_person * self.quant
+        total_cost = self.meat_price * quant_to_be_bought
+        price_per_person = total_cost / self.quant
+
+        Panel(f'''Analisando o [green]{self.name}[\] com [blue]{self.quant}[\] convidados:
+        Cada participante comerá {self.consumption_per_person}kg e cada kg custa R${self.meat_price:.2f}.
+        Recomendo comprar [blue]{quant_to_be_bought:.3f}kg[\] de carne.
+        O custo total será de [green]R${total_cost:.2f}[\].
+        Cada pessoa pagará [yellow]R${price_per_person:.2f}[\] para participar.''', title=self.name)
