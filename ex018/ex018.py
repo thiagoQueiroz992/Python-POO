6c1749500f81c1747978c15c050db706e722fca7
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-16 09:31:47 -0300

    exercise ended

--- ex018/ex018.py
+++ ex018/ex018.py
@@ -14,8 +14,11 @@ class Churrasco():
         total_cost = self.meat_price * quant_to_be_bought
         price_per_person = total_cost / self.quant
 
-        Panel(f'''Analisando o [green]{self.name}[\] com [blue]{self.quant}[\] convidados:
-        Cada participante comerá {self.consumption_per_person}kg e cada kg custa R${self.meat_price:.2f}.
-        Recomendo comprar [blue]{quant_to_be_bought:.3f}kg[\] de carne.
-        O custo total será de [green]R${total_cost:.2f}[\].
-        Cada pessoa pagará [yellow]R${price_per_person:.2f}[\] para participar.''', title=self.name)
+        print(Panel(f'''Analisando o [green]{self.name}[/] com [blue]{self.quant}[/] convidados:
+Cada participante comerá {self.consumption_per_person}kg e cada kg custa R${self.meat_price:.2f}.
+Recomendo comprar [blue]{quant_to_be_bought:.3f}kg[/] de carne.
+O custo total será de [green]R${total_cost:.2f}[/].
+Cada pessoa pagará [yellow]R${price_per_person:.2f}[/] para participar.''', title=self.name))
+
+churras = Churrasco('Domingão na Brasa', 30)
+churras.analysis()
