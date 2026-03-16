91f1561ad42a43c06ff1cc795c3906b88f394cf5
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-16 08:56:52 -0300

    fixing some things and ending

--- ex017/ex017_product.py
+++ ex017/ex017_product.py
@@ -1,14 +1,19 @@
-#from rich import print
+from rich import print
 from rich.panel import Panel
 from rich.align import Align
 
-#class Product():
-#    def __init__(self, name: str, price: float):
-#        self.name = name
-#        self.price = price
-#
-#    def label(self):
-#        Panel(Align(str(self.name).center(50) + '\n' + '-' * 50 + '\n' + f'R${self.price:^-50.2f}', align='center'), width=50)
-#
+class Product():
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
 
-print(f'R${34:^-50.2f}')
+    def label(self):
+        product_label = Panel(Align(self.name.center(50) + '\n' + '-' * 50 + '\n' + f'R${self.price:,.2f}'.center(50), 'center'), title='Product')
+        print(product_label)
+
+p1 = Product('iPhone 17 Pro Max', 23_000)
+p2 = Product('Mangá Vinland Saga', 45)
+p3 = Product('Monitor Gamer 240hz FHD', 1_200)
+p1.label()
+p2.label()
+p3.label()
