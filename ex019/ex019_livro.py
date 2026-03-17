ae9c1711a92e1db4544ad7f8935a9ca0d4f76dc3
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-16 21:10:52 -0300

    exercise ended

--- ex019/ex019_livro.py
+++ ex019/ex019_livro.py
@@ -1,4 +1,5 @@
 from rich import print
+from time import sleep
 
 class Book():
     def __init__(self, book_name: str, pages: int):
@@ -6,21 +7,24 @@ class Book():
         self.pages = pages
         self.current_page = 1
 
+        print(f':book: [blue]Você acabou de abrir o livro [green bold]\'{self.book_name}\'[/] que tem [red bold]{self.pages} páginas[/] no total. Você está na [yellow bold]página {self.current_page}[/].[/]')
+        sleep(1)
+
     def advance_pages(self, pages = 1):
-        pages_advanced = 0
+        print()
         for p in range(pages):
-            if pages_advanced == pages:
-                print(f'Você avançou {pages_advanced} páginas e agora está na página {self.current_page}.')
+            self.current_page += 1
+            print(f'Página {self.current_page}', end=' > ', flush=True)
+            sleep(.5)
+            if self.current_page == self.pages:
                 break
-            else:
-                self.current_page += 1
-                pages_advanced += 1
-                print(f'Página {self.current_page}')
-        if self.current_page >= self.pages:
-            print(f'Você chegou ao final do livro {self.book_name}')
+        print(f'[blue]Você avançou [cyan bold]{p + 1} {'páginas' if p + 1 > 1 else 'página'}[/] e agora está na [yellow bold]página {self.current_page}[/].[/]', flush=True)
+        sleep(1.2)
+        if self.current_page == self.pages:
+            print(f':closed_book: [red]Você chegou ao final do livro [bold]\'{self.book_name}\'[/].[/]')
 
 book = Book('Teste', 20)
-book.advance_pages(3)
-book.advance_pages(5)
+book.advance_pages(2)
 book.advance_pages()
-book.advance_pages(30)
+book.advance_pages(8)
+book.advance_pages(11)
