a0df5ffa0f6753ff65935de35d91eeff8d5da1e6
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-17 09:34:35 -0300

    raw program ready

--- ex020/ex020_gamer.py
+++ ex020/ex020_gamer.py
@@ -1,7 +1,27 @@
 from rich import print
-from rich.panel import panel
+from rich.panel import Panel
 
 class Gamer():
     def __init__(self, real_name: str, nick: str):
         self.real_name = real_name
         self.nick = nick
+        self.favorites = list()
+    
+    def add_favorite(self, game: str):
+        self.favorites.append(game)
+        self.favorites.sort()
+    
+    def gamer_record(self):
+        record = Panel(f'Nome real: {self.real_name}\nJogos favoritos:')
+        for g in self.favorites:
+            record.renderable += '\n' + g
+        print(record)
+
+g1 = Gamer('Thiago Queiroz', 'kert_o.neihl')
+g1.add_favorite('Minecraft')
+g1.add_favorite('Genshin Impact')
+g1.add_favorite('Albion Online')
+g1.add_favorite('World of Warcraft')
+g1.add_favorite('Forza Horizon')
+g1.add_favorite('Counter Strike')
+g1.gamer_record()
