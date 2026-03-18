20d3c8b5cf35f82af7b2a7b1ec54ed6c82dedd82
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-17 21:02:03 -0300

    some adjusts and now tv powers on and off

--- ex022/ex022_controle_remoto.py
+++ ex022/ex022_controle_remoto.py
@@ -5,9 +5,21 @@ class RemoteControl():
     def __init__(self):
         self.channel = 1
         self.volume = 1
-        self.is_tv_on = False
-        self.tv_panel = Panel('TV')
-        print('Hello World')
+        self.tv_panel = Panel('TV is off', title='[ TV ]', width=40)
+        self.input = ''
+        print(self.tv_panel)
+        if self.channel_and_volume() == '@':
+            self.power_on()
+    
+    def channel_and_volume(self):
+        self.input = str(input('< CH >   < VOL > ::  '))
+        return self.input
+
+    def power_on(self):
+        self.tv_panel.renderable = 'CHANNEL\nVOLUME'
+        print(self.tv_panel)
+        if self.channel_and_volume() == '@':
+            self.__init__()
     
 
 rc = RemoteControl()
